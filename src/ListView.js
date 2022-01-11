cb34b02845a6f1658b4b86e388d5538a0f090526
Reinis <rlukins123@gmail.com>
2022-01-11 21:34:10 +0200

    Add FlatList, add element option

--- src/ListView.js
+++ src/ListView.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, Platform, FlatList, Button } from 'react-native';
 import { Appbar, List, Searchbar } from 'react-native-paper';
 import Constants from 'expo-constants';
 
 const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
 import DdMenu from './dd-menu';
+import { Component } from 'react/cjs/react.production.min';
 
 export default function ListView({ navigation }) {
   const [showDD, setShowDD] = React.useState(false)
+  const [initialElements, changeEl] = useState([
+    { id: "1", name: "Zeķes"},
+    { id: "2", name: "Bumba"}
+  ]);
+
+  const [exampleState, setExampleState] = useState(initialElements);
+  const [idx, incr] = useState(2);
+
+  const addElement = () => {
+    var newArray = [...initialElements , {id : idx, text: "Object " + (idx+1) }];
+    incr(idx + 1);
+    console.log(initialElements.length);
+    setExampleState(newArray);
+    changeEl(newArray);
+  }
 
   return (
     <View>
@@ -19,6 +35,13 @@ export default function ListView({ navigation }) {
       <Appbar.Action icon={MORE_ICON} onPress={() => { showDD ? <DdMenu /> : null }} />
     </Appbar.Header>
     <Searchbar placeholder="Search" />
+    <FlatList
+            keyExtractor = {item => item.id}  
+            data={exampleState}
+            renderItem = {item => (<Text>{item.item.text}</Text>)} />
+        <Button
+          title="Add element"
+          onPress={addElement} />
     </View>
   );
 }
