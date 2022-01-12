110f7061b6ff9443e29a728070b4acc69536f085
Reinis <rlukins123@gmail.com>
2022-01-12 12:56:17 +0200

    Clean up code, add OnPress, add separator lines

--- src/ListView.js
+++ src/ListView.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
 import { Text, View, StyleSheet, Platform, FlatList, Button } from 'react-native';
 import { Appbar, List, Searchbar } from 'react-native-paper';
 import Constants from 'expo-constants';
@@ -8,26 +7,28 @@ import objects from "./data.json"; // Data file for flatlist
 const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
 import DdMenu from './dd-menu';
-//import { Item } from 'react-native-paper/lib/typescript/components/List/List';
 
 export default function ListView({ navigation }) {
   const [showDD, setShowDD] = React.useState(false)
 
   const [objectData, setObjectData] = React.useState(objects);
-  const ItemView = ({item}) => {
-    return (
+  const ItemView = ({item}) => { return (
       <View>
-        <Text>{item.name}</Text>
-        <Text>{item.location}</Text>
+        <Text onPress={() => onItemClick(item)}>{item.name}, atrašanās vieta: {item.location}</Text>
       </View>
-    )
+  )}
+
+  const ItemSeparatorView = () => {
+    return (
+      <View
+        style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}
+      />
+    );
+  };
+
+  const onItemClick = (item) => {
+    alert('Objekts: '+item.name+', atrašanās vieta: '+item.location);
   }
-  const Item = ({name, location}) => (
-    <View>
-      <Text>{name}</Text>
-      <Text>{location}</Text>
-    </View>
-  )
 
   const addElement = () => {
     alert("Šeit pievieno jaunu ierakstu")
@@ -35,21 +36,22 @@ export default function ListView({ navigation }) {
 
   return (
     <View>
-    <Appbar.Header
-      theme={{ colors: { primary: '#3E658A'} }}>
-      <Appbar.Content title="Item list" />
-      <Appbar.Action icon="magnify" onPress={() => {}} />
-      <Appbar.Action icon={MORE_ICON} onPress={() => { showDD ? <DdMenu /> : null }} />
-    </Appbar.Header>
-    <Searchbar placeholder="Search" />
-    <Button
-          title="Pievienot jaunu ierakstu"
-          onPress={addElement} />
-    <FlatList
-            keyExtractor = {item => item.id}  
-            data={objectData}
-            renderItem = {ItemView}
-    />
+      <Appbar.Header theme={{ colors: { primary: '#3E658A'} }}>
+        <Appbar.Content title="Item list" />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon={MORE_ICON} onPress={() => { showDD ? <DdMenu /> : null }} />
+      </Appbar.Header>
+      <Searchbar placeholder="Search" />
+      <Button
+        title="Pievienot jaunu ierakstu"
+        onPress={addElement}
+      />
+      <FlatList
+        ItemSeparatorComponent={ItemSeparatorView}
+        keyExtractor = {item => item.id}  
+        data={objectData}
+        renderItem = {ItemView}
+      />
     </View>
   );
 }
